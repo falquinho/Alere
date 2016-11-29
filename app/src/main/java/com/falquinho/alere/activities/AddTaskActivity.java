47509dc6f1cf03d8d31d4ae481a1856d4bdc3a08
falquinho <lucas.falcao.nb@gmail.com>
2016-11-29 09:34:38 -0200

    Várias mudanças e adiçoes feitas

--- app/src/main/java/com/falquinho/alere/activities/AddTaskActivity.java
+++ app/src/main/java/com/falquinho/alere/activities/AddTaskActivity.java
@@ -2,13 +2,16 @@ package com.falquinho.alere.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.util.Log;
+import android.view.View;
 
 import com.falquinho.alere.R;
 
 /**
  * Created by falquinho on 24/11/2016.
  */
-public class AddTaskActivity extends Activity
+public class AddTaskActivity extends Activity implements View.OnClickListener
 {
     @Override
     public void onCreate(Bundle savedInstance)
@@ -17,5 +20,13 @@ public class AddTaskActivity extends Activity
         setContentView(R.layout.add_task_layout);
 
         setTitle("Add New Task");
+
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab_done_add_task);
+        fab.setOnClickListener(this);
+    }
+
+    public void onClick(View v)
+    {
+        Log.i("AddTaskActivity","FAB Clicked");
     }
 }
--- app/src/main/java/com/falquinho/alere/fragments/TaskFragment.java
+++ app/src/main/java/com/falquinho/alere/fragments/TaskFragment.java
@@ -1,7 +1,40 @@
 package com.falquinho.alere.fragments;
 
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.falquinho.alere.R;
+
 /**
  * Created by falquinho on 24/11/2016.
  */
-public class TaskFragment {
+
+public class TaskFragment extends ListFragment implements View.OnClickListener
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance)
+    {
+        return inflater.inflate(R.layout.task_fragment_layout, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstance)
+    {
+        super.onActivityCreated(savedInstance);
+
+        FloatingActionButton fab;
+        fab = (FloatingActionButton) getActivity().findViewById(R.id.fab_add_task);
+        fab.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        Log.i("TaskFragment","Add Task FAB Clicked");
+    }
 }
